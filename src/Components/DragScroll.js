e9537c26e691badc2e78fbfb2b80b36e90eebad0
Justin Kwan <jkwan19@gmail.com>
2021-06-14 15:27:01 -0700

    Display date with scroll

--- src/Components/DragScroll.js
+++ src/Components/DragScroll.js
@@ -1,29 +1,45 @@
 import React, {
   useState,
+  useEffect,
 } from 'react';
 import './DragScroll.css';
 
-const calcProgress = (min, max) => {
-  return (max - min) * 100;
+
+const dateFromDay = (year, day) => {
+  const currentYear = new Date(year, 0);
+  const currentDate = new Date(currentYear.setDate(day));
+  const options = {
+    month: 'long'
+  }
+  const month = new Intl.DateTimeFormat('en-US', options).format(currentDate)
+  const date = currentDate.getDate();
+  return `${month.substring(0, 3)} ${date}` ;
 }
 const DragScroll = () => {
 
   const [value, setValue] = useState(1);
+  const [date, setDate] = useState(dateFromDay(2021, 1))
 
-  const handleScroll = (min, max) => {
-    setValue(calcProgress(min, max));
+  useEffect(() => {
+    setDate(dateFromDay(2021, value))
+  }, [value])
+
+  const handleScroll = (event) => {
+    setValue(event.target.value)
   };
 
   return (
     <div>
       <div className="forecast">GROWTH FORECAST</div>
-      <div className="date">Jan 1</div>
+      <div className="date">{date}</div>
       <div className="slider">
         <input
           type="range"
           min={1}
           max={365}
           value={value}
+          step={1}
+          onChange={handleScroll}
           />
         <label htmlFor="jan" className="drag-left">Jan</label>
         <label htmlFor="dec" className="drag-right">Dec</label>
