b6f96ecc84cd656b74e9f7fffb820df0aa509562
Justin Kwan <jkwan19@gmail.com>
2021-06-13 23:28:35 -0700

    Add house and plant to context

--- src/Context/context.js
+++ src/Context/context.js
@@ -1,17 +1,36 @@
-import React, { useState, createContext } from 'react';
+import React, {
+  useEffect,
+  useState,
+  createContext
+} from 'react';
+
+const plants = ['Basil', 'Beets', 'Scallion'];
+const houses = ["House", "Apartment", "Town House"];
 
 export const Context = createContext();
 
 export function ContextProvider (props) {
   const [ houseActiveIndex, setHouseActiveIndex ] = useState(0);
   const [ plantActiveIndex, setPlantActiveIndex ] = useState(0);
+  const [ plant, setPlant ] = useState(plants[0]);
+  const [ house, setHouse ] = useState(houses[0]);
+
+  useEffect(() => {
+    setPlant(plants[plantActiveIndex]);
+  }, [plantActiveIndex])
+
+  useEffect(() => {
+    setHouse(houses[houseActiveIndex]);
+  }, [houseActiveIndex])
 
   return(
     <Context.Provider value={{
       houseActiveIndex,
       setHouseActiveIndex,
       plantActiveIndex,
-      setPlantActiveIndex
+      setPlantActiveIndex,
+      plant,
+      house
     }}>
       {props.children}
     </Context.Provider>
